ff1cb2083cc3a63d0c4574a22ff92c9fe7de8572
lpzju <liupeng.hust@foxmail.com>
2023-02-23 22:34:23 +0800

    头插法

--- runoob/lianbiao13.c
+++ runoob/lianbiao13.c
@@ -10,12 +10,45 @@ typedef struct _list {
     Node *head;
 } List;
 
+void add(List *,int);
+void print(List *list);
+
 int main(){
     List list;
     list.head = NULL;
     int number;
     char ch;
-    while (true) {
+    do {
+        printf("请输入一个数字，输入-1表示结束:\n");
+        scanf("%d",&number);
+        if (number != -1) {
+            add(&list,number);
+        }
+    } while(number != -1);
+    print(&list);
+    return 0;
+}
 
+void add(List *list, int number){
+    Node *p = (Node *) malloc (sizeof(Node));
+    p -> value = number;
+    p -> next = NULL;
+    Node *last = list -> head;
+    if (last) {
+        while (last -> next) {
+            last = last -> next;
+        }
+        last -> next = p;
+    }else {
+        list -> head = p;
     }
+}
+
+void print(List *list){
+    Node *temp = list -> head;
+    while (temp -> next) {
+        printf("%d ",temp -> value);
+        temp = temp -> next;
+    }
+    printf("%d\n",temp -> value);
 }
--- xiaojiayu/p45.c
+++ xiaojiayu/p45.c
@@ -14,11 +14,24 @@ void getInput(struct Book *book){
     scanf("%s", book -> author);
 }
 
+void printBook(struct Book *library){
+    struct Book *book;
+    int count = 1;
+    book = library;
+    while (book != NULL) {
+        printf("Book%d：",count);
+        printf("书名：%s\n",book -> title);
+        printf("作者：%s\n",book -> author);
+        book = book -> next;
+        count++;
+    }
+}
+
 void addBook(struct Book **library){
     struct Book *book, *temp;
 
     book = (struct Book *) malloc (sizeof(struct Book));
-    if(book){
+    if(book == NULL){
         printf("内存分配失败!\n");
         exit(1);
     }
@@ -36,5 +49,6 @@ void addBook(struct Book **library){
 int main(){
     struct Book *library = NULL;
     addBook(&library);
+    printBook(library);
     return 0;
 }
