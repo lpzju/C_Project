abf411ea886bd7aebf9268120298ff696403824b
lpzju <liupeng.hust@foxmail.com>
2023-02-25 11:27:34 +0800

    小甲鱼关于栈的内存增长不完全对

--- xiaojiayu/p39.c
+++ xiaojiayu/p39.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// 注意不能直接认为栈是从高到低排列
+// 经测试，win11和centos8的栈是从高到低，ubuntu20的栈是从低到高
+// 其他系统未测试，那么其实应该是系统进行了调整
+
 int main(){
     // 堆和栈的不同内存
     int *ptr1 = (int *) malloc (4 * sizeof (int));
