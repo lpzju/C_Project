f2a258db51a72604374c8166c2e6fff39bb4f943
lpzju <liupeng.hust@foxmail.com>
2023-02-26 21:02:07 +0800

    0226

---
+++ xiaojiayu/baifen.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    char *str;
+    scanf("%s",str);
+    printf("%n\n",(int *)str);
+    return 0;
+}
---
+++ xiaojiayu/sqrt1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+double func1(double x){
+    return 1.1;
+}
+
+int main(){
+    int res = func1(1.1);
+    printf("%d\n",res);
+    return 0;
+}
